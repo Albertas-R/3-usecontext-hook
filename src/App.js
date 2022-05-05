26247e7ce5b839666d46ab16b1c8390c13fd17a4
Albertas <albertas.rinkevicius@gmail.com>
2022-05-05 09:58:46 +0300

    multiple context values

--- src/App.js
+++ src/App.js
@@ -8,13 +8,30 @@ import React from "react";
 import "./App.css";
 import ComponentC from "./components/ComponentC";
 
+// one context value
+// export const UserContext = React.createContext();
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <UserContext.Provider value={'Albertas'}>
+//         <ComponentC />
+//       </UserContext.Provider>
+//     </div>
+//   );
+// }
+
+// multiple context values
 export const UserContext = React.createContext();
+export const AnotherContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'Albertas'}>
-        <ComponentC />
+      <UserContext.Provider value={"Albertas"}>
+        <AnotherContext.Provider value={"Rinkevičius"}>
+          <ComponentC />
+        </AnotherContext.Provider>
       </UserContext.Provider>
     </div>
   );
--- src/components/ComponentF.js
+++ src/components/ComponentF.js
@@ -1,13 +1,36 @@
 import React from "react";
-import { UserContext } from "../App";
+import { UserContext, AnotherContext } from "../App";
 
 // one context value
+// function ComponentF() {
+//   return (
+//     <div>
+//       <UserContext.Consumer>
+//         {(user) => {
+//           return <div>UserContext value: {user}</div>;
+//         }}
+//       </UserContext.Consumer>
+//     </div>
+//   );
+// }
+
+// multiple context values
 function ComponentF() {
   return (
     <div>
       <UserContext.Consumer>
         {(user) => {
-          return <div>UserContext value: {user}</div>;
+          return (
+            <AnotherContext.Consumer>
+              {(anotherValue) => {
+                return (
+                  <div>
+                    UserContext value: {user}; AnotherContext value: {anotherValue}
+                  </div>
+                );
+              }}
+            </AnotherContext.Consumer>
+          );
         }}
       </UserContext.Consumer>
     </div>
